df3f62e5c407f9e8b1aae5d9b5a8929813541538
AliAlsaeed176 <alialsaeed176@gmail.com>
2023-01-26 03:29:09 -0800

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
+      statusBarColor: Colors.transparent,
+      statusBarIconBrightness: Brightness.dark,
+    ));
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: "Social Media Application",
+    );
+  }
+}
